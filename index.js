456e67239a5a66a6580682e23e7eb5f51ac15fb8
saleszera <crowzcoleslaw@gmail.com>
2019-11-09 00:21:17 -0300

    definindo movimentacao da snake

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ snake[0] = {
     y: 8*box
 }
 
+let direction = 'right'
+
 function createBG(){
     context.fillStyle = 'lightgreen'
     context.fillRect(0, 0, 16*box, 16*box)
@@ -20,5 +22,28 @@ function createSnake(){
         context.fillRect(snake[i].x, snake[i].y, box, box)
 }
 }
-createBG()
-createSnake()
+
+function startGame(){
+
+    createBG()
+    createSnake()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if(direction == 'right') snakeX += box;
+    if(direction == 'left') snakeX -= box;
+    if(direction == 'up') snakeY -= box;
+    if(direction == 'down') snakeY += box;
+
+    snake.pop()
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+let game = setInterval(startGame, 100)
